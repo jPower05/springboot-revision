c0560760f6c625a388cd5879f394f6878e6fa91b
jPower05 <jamespbaaah05@gmail.com>
2022-06-27 14:22:16 +0100

    Initial commit

---
+++ src/test/java/com/james/springbootrevision/SpringbootRevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.springbootrevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
